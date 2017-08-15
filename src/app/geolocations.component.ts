39e26213350e84ceb043396811c30a891a467cb8
avoiteh <avoiteh@gmail.com>
2017-08-15 09:58:09 +0400

    i15.08 1

--- src/app/geolocations.component.ts
+++ src/app/geolocations.component.ts
@@ -54,7 +54,9 @@ export class geoPointsTreeViewDirective implements OnChanges {
  }
 
  refresh(){
-  this.elementRef.nativeElement.innerHTML = this.drawTree(this.pointService.getData());
+  let newNodes=this.filterTree(this.pointService.getData(), this.pointService.Search);
+  console.log(newNodes);
+  this.elementRef.nativeElement.innerHTML = this.drawTree(newNodes);
  }
 
  drawTree(nodes){
@@ -66,24 +68,36 @@ export class geoPointsTreeViewDirective implements OnChanges {
     let inner_html='';
  	 	for(let i in nodes){
  	 		subHtml = this.drawTree(nodes[i].nodes);
- 	 		if(nodes[i].text.toLowerCase().indexOf(this.pointService.Search.toLowerCase()) > -1 ||
-         subHtml != ''){
+
  	 			inner_html += '<li>';// class="well well-sm"
         if(subHtml!=''){
           inner_html+='<a class="btn btn-success btn-xs"><span class="glyphicon '+(nodes[i].collapsed ? 'glyphicon-chevron-right':'glyphicon-chevron-down')+'" treetype="collapse" nodeId="'+nodes[i].id+'"></span></a>';
         }
         inner_html += '<span treetype="check" nodeId="'+nodes[i].id+'" class="glyphicon '+(nodes[i].check ? 'glyphicon-check':'glyphicon-unchecked')+' " style="font-size:20px;"></span>';
-        //html += '<input type="checkbox" treetype="check" nodeId="'+nodes[i].id+'" '+(nodes[i].check ? 'checked':'')+'>';
         inner_html += nodes[i].text+'</li>';
         if(!nodes[i].collapsed){
  	 				inner_html += subHtml;
  	 			}
- 	 		}
+
  	 	}
     if(inner_html != ''){
       html+='<ul style="list-style-type: none;">'+inner_html+'</ul>';
     }
  	}
  	return html;
+ }
+
+
+ filterTree(nodes, Search){
+  let newNodes=[];
+  let j=0;
+  for(let i in nodes){
+    let items=this.filterTree(nodes[i].nodes, Search);
+    if(items.length > 0 || nodes[i].text.toLowerCase().indexOf(Search.toLowerCase()) > -1 ){
+      newNodes[j]={"id":nodes[i].id, "text":nodes[i].text,"check":nodes[i].check,"collapsed":nodes[i].collapsed,"nodes":items};
+      j++;
+    }
+  }
+  return newNodes;
  }
 }
